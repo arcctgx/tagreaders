d1fc6173b3ed400dd87e63b4dfecf615165b2ed9
arcctgx <arcctgx@poczta.onet.pl>
2020-06-14 02:15:28 +0200

    introduce bitratechecker class, improve output

--- bitratechecker.py
+++ bitratechecker.py
@@ -6,23 +6,56 @@ import glob
 import subprocess
 import json
 
+class bitratechecker(object):
+    def __init__(self, file_list):
+        self.__name = []
+        self.__bitrate = []
+        self.__mode = []
+        self.__library = []
+        self.__encoder = []
+
+        raw_json = subprocess.check_output(["mediainfo", "--output=JSON"] + sorted(file_list))
+        media = json.loads(raw_json)
+
+        for m in media:
+            self.__name.append(os.path.basename(str(m["media"]["@ref"])))
+
+            try:
+                self.__bitrate.append(int(m["media"]["track"][0]["OverallBitRate"]))
+            except KeyError:
+                self.__bitrate.append(-1)
+
+            try:
+                self.__mode.append(str(m["media"]["track"][0]["OverallBitRate_Mode"]))
+            except KeyError:
+                self.__mode.append("unknown")
+
+            try:
+                self.__library.append(str(m["media"]["track"][0]["Encoded_Library"]))
+            except KeyError:
+                self.__library.append("unknown")
+
+        for f in sorted(file_list):
+            try:
+                enc = subprocess.check_output(["mp3guessenc", "-n", f]).splitlines()[0]
+            except subprocess.CalledProcessError, e:
+                # mp3guessenc returns different statuses for different encoders - not just zero on success
+                # so we actually get meaningful output here in except block
+                enc = e.output.splitlines()[0]
+
+            self.__encoder.append(enc)
+
+    def print_formatted(self):
+        s = []
+
+        for z in zip(self.__name, self.__bitrate, self.__mode, self.__library, self.__encoder):
+            s.append("%-50s\t%d\t%-12s\t%-20s\t%s" % (z[0], z[1], z[2], z[3], z[4]) )
+
+        print '\n'.join(s)
+
 def usage():
     print "usage: " + os.path.basename(sys.argv[0]) + " <dir> [dir2] ..."
     sys.exit(1)
-
-def get_encoder(file_list):
-    encoder = []
-
-    for f in sorted(file_list):
-	# mp3guessenc returns different statuses for different encoders...
-        try:
-            enc = subprocess.check_output(["mp3guessenc", "-n", f]).splitlines()[0]
-        except subprocess.CalledProcessError, e:
-            enc = e.output.splitlines()[0]
-
-        encoder.append(enc)
-
-    return encoder
 
 
 if len(sys.argv) < 2:
@@ -32,39 +65,6 @@ for path in sys.argv[1:]:
     print path
 
     mp3_files = glob.glob(os.path.join(path, "*.mp3"))
-
-    raw_json = subprocess.check_output(["mediainfo", "--output=JSON"] + sorted(mp3_files))
-    media = json.loads(raw_json)
-
-    name = []
-    bitrate = []
-    mode = []
-    library = []
-
-    for m in media:
-        name.append(os.path.basename(str(m["media"]["@ref"])))
-
-        try:
-            bitrate.append(int(m["media"]["track"][0]["OverallBitRate"]))
-        except KeyError:
-            bitrate.append(-1)
-
-        try:
-            mode.append(str(m["media"]["track"][0]["OverallBitRate_Mode"]))
-        except KeyError:
-            mode.append("unknown")
-
-        try:
-            library.append(str(m["media"]["track"][0]["Encoded_Library"]))
-        except KeyError:
-            library.append("unknown")
-
-    print name
-    print bitrate
-    print mode
-    print library
-
-    encoder = get_encoder(mp3_files)
-    print encoder
-
+    x = bitratechecker(mp3_files)
+    x.print_formatted()
     print
